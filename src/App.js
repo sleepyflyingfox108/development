83284678216c56fda6904418fd9abefd31a392f2
sleepyflyingfox108 <114830242+sleepyflyingfox108@users.noreply.github.com>
2022-12-02 13:32:03 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,72 @@
+import "./App.css";
+import { useState } from "react";
+import cityData from "./assets/asian-cities-data.json";
+import City from "./components/City.js";
+import Filters from "./components/Filters.js";
+
+/* ####### DO NOT TOUCH -- this makes the image URLs work ####### */
+cityData.forEach((item) => {
+  item.image = process.env.PUBLIC_URL + "/" + item.image;
+});
+/* ############################################################## */
+
+function App() {
+  const [populationSort, setPopulationSort] = useState(false)
+  const [currRegion, setRegion] = useState("All")
+  const [currReligions, setReligions] = useState({})
+  const [favorites, setFavorites] = useState({})
+  const [displayFavorites, toggleDisplayFavorites] = useState(false)
+
+  const matchesRegion = (city) => {
+    if (currRegion === "All") {
+      return true
+    } else if (city.region === currRegion) {
+      return true
+    } else {
+      return false
+    }
+  }
+
+  const matchesReligion = (city) => {
+    var matches = true;
+    {Object.keys(currReligions).map((item, index) => ( 
+      (!city.religions.includes(item) ? matches = false : null)
+    ))}
+    return matches
+
+  }
+
+  var data = null
+
+  if (displayFavorites) {
+    data = Object.keys(favorites).map((key, value) => (favorites[key]))
+  } else {
+    data = cityData
+  }
+
+  var filteredData = data.filter(matchesReligion).filter(matchesRegion)
+  if (populationSort) {
+    filteredData = filteredData.sort((a, b) => {return b.population - a.population})
+  }
+
+  return (
+    <div className="App">
+      <h1>Cities in Asia</h1>
+      <div id="content">
+        <div id="gallery">
+
+        {filteredData.map((item, index) => (
+          <div id="city-city">
+            <City key={index} item={item} favorites={favorites} setFavorites={setFavorites} />
+            <br></br>
+          </div>
+        ))}
+        </div>
+
+        <Filters setPopulationSort={setPopulationSort} setRegion={setRegion} currReligions={currReligions} setReligions={setReligions} displayFavorites={displayFavorites} toggleDisplayFavorites={toggleDisplayFavorites} favorites={favorites} />
+        </div>
+    </div>
+  );
+}
+
+export default App;
